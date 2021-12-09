aca85d63e7047318a098afd106dd333dd3360b0f
dev0x01 <95337330+dev0x01@users.noreply.github.com>
2021-12-09 23:30:43 +0200

    Fhh

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-<!-- Matomo -->
-<script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://thirstyaustin8af7ffnetlifyapp.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='//cdn.matomo.cloud/thirstyaustin8af7ffnetlifyapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->        <title>Next.js Starter!</title>
+        <title>Next.js Starter!</title>
+<script src="https://plausible.io/js/plausible.js"></script>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
